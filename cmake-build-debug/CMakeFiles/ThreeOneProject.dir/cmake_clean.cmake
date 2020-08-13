c3ff9e1e165f427374b310e0a26ccd9f71303177
bgst009 <1499135121@qq.com>
2020-08-13 16:40:00 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ThreeOneProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThreeOneProject.dir/main.c.o"
+  "ThreeOneProject.exe"
+  "ThreeOneProject.pdb"
+  "libThreeOneProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ThreeOneProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
