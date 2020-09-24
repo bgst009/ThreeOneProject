f2f2ec31678b73394a5e3be34ef1fc468c8a9eb8
bgst009 <1499135121@qq.com>
2020-09-24 10:21:22 +0800

    改变文档结构

--- 31prj_c/a.c
+++ 31prj_c/a.c
@@ -1,1 +1,8 @@
-main() { printf("hello world!\n"); }
+int a;
+void f1(void) { a = 1; }
+void f2(void) { a = 2; }
+void f3(void) { a = 3; }
+main() {
+    printf("%X\n", _CS);
+    printf("%x:%x\n", &f1, f1);
+}
