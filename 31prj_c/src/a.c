06cd221abbaa58d386e2c174f160bf5466f87e87
bgst009 <1499135121@qq.com>
2020-09-24 10:52:41 +0800

    改变文档结构

--- 31prj_c/src/a.c
+++ 31prj_c/src/a.c
@@ -3,6 +3,6 @@ void f1(void) { a = 1; }
 void f2(void) { a = 2; }
 void f3(void) { a = 3; }
 main() {
-    printf("%X\n", _CS);
+    /*printf("%X\n", _CS);*/
     printf("%x:%x\n", &f1, f1);
 }
--- main.c
+++ main.c
@@ -1,3 +1,8 @@
 //
 // Created by bgst on 2020/9/23.
 //
+
+#include"./include/header.h"
+int main(){
+  printf ("Hello World\n");
+}
