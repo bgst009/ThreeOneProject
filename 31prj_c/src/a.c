10533e0de9257e9f9c2bd619c6f1428c69811df1
bgst009 <1499135121@qq.com>
2020-09-24 13:46:27 +0800

    update Doc

--- 31prj_c/src/a.c
+++ 31prj_c/src/a.c
@@ -3,6 +3,10 @@ void f1(void) { a = 1; }
 void f2(void) { a = 2; }
 void f3(void) { a = 3; }
 main() {
-    /*printf("%X\n", _CS);*/
-    printf("%x:%x\n", &f1, f1);
+    char *string = "--------------------";
+    printf("\nCS: %x\n", _CS);
+    printf("%s", string);
+    printf("\nf1: %x\n", f1);
+    printf("\nf2: %x\n", f2);
+    printf("\nf3: %x\n", f3);
 }
