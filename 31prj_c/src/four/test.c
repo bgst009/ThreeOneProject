16840d87d31dd27208426410fe44899191e37976
bgst009 <1499135121@qq.com>
2020-10-05 11:41:37 +0800

    done 研究实验4-5

---
+++ 31prj_c/src/four/test.c
@@ -0,0 +1,11 @@
+int t1(int a, int b) {
+    int c = 9;
+    c = a + b;
+    return c;
+}
+int t2(int a, int b) {
+    int c = 8;
+    c = a - b;
+    return c;
+}
+int t3(int a, int b) { return a + b + 1; }
