8bb565b710df573599140460b75d559fd844c1c3
bgst009 <1499135121@qq.com>
2020-10-14 23:18:34 +0800

    8,9宣讲会

--- 31prj_c/src/08_eight/a.c
+++ 31prj_c/src/08_eight/a.c
@@ -3,17 +3,25 @@ struct stu {
     unsigned char os;
     unsigned char masm;
     unsigned char java;
-    struct stu *next;
+    struct stu far *next;
 };
-
+void f1();
+void f2();
 int n;
 struct stu a[375];
-struct stu *s;
+struct stu *s = 0x00000000;
 int sum;
 main() {
+    system("cls");
+    f2();
+}
+
+void f1() {
     long address = 0x00000000;
     printf("%lx", address);
 
+    printf(" %d", sizeof(unsigned char));
+    sleep(1);
     for (n = 0; n < 375; n++) {
         a[n].c = *(char far *)address++;
         a[n].os = *(char far *)address++;
@@ -41,7 +49,36 @@ main() {
             break;
         printf("%d: ", ++n);
         sum = s->c + s->os + s->masm + s->java;
-        printf("c: %c,os: %c,masm: %c,java: %c,sum: %d\n", s->c, s->os, s->masm,
-               s->java, sum);
+        printf("c: %u,os: %u,masm: %u,java: %u,sum: %d \n", s->c, s->os,
+               s->masm, s->java, sum);
+        /* sleep(1); */
+    }
+}
+void f2() {
+    system("cls");
+    for (n = 0; n < 375; n++) {
+        a[n] = *s++;
+        printf("%x ", s);
+        sleep(1);
+    }
+    s->next = 0;
+    for (n = 0; n < 375; n++) {
+        if (a[n].c + a[n].os + a[n].masm + a[n].java < 400 &&
+            a[n].c + a[n].os + a[n].masm + a[n].java > 200) {
+            a[n].next = s->next;
+            s->next = &a[n];
+        }
+    }
+
+    n = 0;
+    while (s->next) {
+        s = s->next;
+        if (!s)
+            break;
+        printf("%d: ", ++n);
+        sum = s->c + s->os + s->masm + s->java;
+        printf("c: %u,os: %u,masm: %u,java: %u,sum: %d \n", s->c, s->os,
+               s->masm, s->java, sum);
+        /* sleep(1); */
     }
 }
--- 31prj_c/src/09_nine/a.c
+++ 31prj_c/src/09_nine/a.c
@@ -1,11 +1,42 @@
+typedef void *va_list;
+
+#define va_start(ap, parmN) (ap = ...)
+#define va_arg(ap, type) (*((type *)(ap))++)
+#define va_end(ap)
+#define _va_ptr (...)
+
 void showstr(char *, ...);
+void showstr2(char *, ...);
+void showstr3(char *, ...);
 char *p1 = "This is a c program.";
 char *p2 = "Welcome to c !";
 char *p3 = "Hello world !";
 int i = 0;
-main() { showstr(p1, p2, p3, 0); }
+main() {
+    /* showstr(p1, p2, p3, 0); */
+    showstr3(p1, p2, p3, 0);
+}
 void showstr(char *str, ...) {
+    /*     printf("%x\n", _BP);
+        printf("%x\n", &str); */
     for (i = 0; (*(int *)(_BP + 4 + i)) != 0; i += 2) {
         printf("%s\n", *(int *)(_BP + 4 + i));
+    }
+}
+
+void showstr2(char *p, ...) {
+    char *ret;
+    va_list ap;
+    va_start(ap, p);
+    ret = p;
+    printf("%s\n", ret);
+    for (ret; ret = va_arg(ap, char *);)
+        printf("%s\n", ret);
+}
+
+void showstr3(char *pstr, ...) {
+    int *p;
+    for (p = &pstr; (char *)*(p); p++) {
+        printf("%s\n", (char *)*(p));
     }
 }
