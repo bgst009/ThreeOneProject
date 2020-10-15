dbb6e7d482afe46018611ed8d09b84f7bb5253a9
bgst009 <1499135121@qq.com>
2020-10-15 23:24:18 +0800

    综合研究18

--- 31prj_c/src/018_eighteen/ml.c
+++ 31prj_c/src/018_eighteen/ml.c
@@ -1,54 +1,41 @@
-#include "list5.h"
-typedef struct student {
-    int number;
-    int math;
-    int c;
-    int java;
-} stu;
-typedef stu MyElementType;
-MyElementType a[8] = {
-    1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4,
-    1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4,
-};
+#include "list1.h"
+
+int a[8] = {11, 22, 33, 44, 55, 66, 77, 88};
 void showlist(List *);
-int putelement(MyElementType *);
+int putelement(EleType *);
 
 main() {
     List *lp;
     int n;
 
-    lp = CreatList(sizeof(MyElementType));
+    lp = CreatList();
     if (!lp) {
         printf("Creat?\n");
         return;
     }
 
     for (n = 0; n < 8; n++) {
-        ListAppend(lp, &(a[n]));
+        ListAppend(lp, a[n]);
     }
     showlist(lp);
 
-/*     n = 8;
-    ListInsert(lp, 1, &n);
+    ListInsert(lp, 1, 8);
     showlist(lp);
-    n = 18;
-    ListInsert(lp, 8, &n);
+    ListInsert(lp, 8, 18);
     showlist(lp);
 
     ListDelet(lp, 1);
     showlist(lp);
     ListDelet(lp, 7);
-    showlist(lp); */
+    showlist(lp);
 }
 
 void showlist(List *lp) {
-    TraverseList(lp, (int (*)(void *))putelement);
+    TraverseList(lp, putelement);
     printf("\n");
 }
 
-int putelement(MyElementType *element) {
-    printf("%d %d %d %d \n", element->number, element->math, element->c,
-           element->java);
-    sleep(1);
+int putelement(EleType *data) {
+    printf("%d ", *data);
     return 1;
 }
